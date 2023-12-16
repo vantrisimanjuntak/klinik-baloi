b7bafdd191eca1f7635949ab01ba160e1cbef981
DESKTOP-7NBOJSE\Random <vantri_antonius@yahoo.co.id>
2023-12-16 10:41:12 +0700

    tambah konten fasilitas rsob

--- application/modules/home/views/main_content.php
+++ application/modules/home/views/main_content.php
@@ -9,8 +9,12 @@
     </div>
 </section>
 
-<div class="container p-3 mb-5 text-white thumb rounded">
+<div class="container p-3 mb-5 thumb rounded">
     <div class="container w-75 pt-4">
-        <h2 class="text-primary font-weight-bold">Fasilitas RSOB</h2>
+        <h2 class="text-primary font-weight-bold pb-4">Fasilitas RSOB</h2>
+        <p>Pelayanan Transfusi Darah</p>
+        <p>Pelayanan Rehabilitas Medik</p>
+        <p>Pelayanan Rekam Medik</p>
+        <p>Pelayanan Rawat Jalan</p>
     </div>
 </div>
