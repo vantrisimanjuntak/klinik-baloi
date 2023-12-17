3fb700087377b6935f013eb0efe79e6758de5315
DESKTOP-7NBOJSE\Random <vantri_antonius@yahoo.co.id>
2023-12-17 23:59:21 +0700

    footer

--- application/modules/home/views/footer.php
+++ application/modules/home/views/footer.php
@@ -5,39 +5,67 @@
                 <img src="<?= base_url('assets/images/logo-ppid.png') ?>" style="max-width:180px" alt="">
                 <p class="text-left pt-3">Dalam rangka mewujudkan open goverment Indonesia dan sebagai implementasi Undang-Undang Nomor 25 Tahun 2009, Badan Pengusaha Batam yang merupakan institusi pemerintah melakukan berbagai cara dan upaya untuk peningkatan pelayanan publik, diantaranya dengan pengelolaan sistem informasi pelayanan publik. Pengelolaan ini dibuat dan dirancang untuk mempermundah komunikasi dan interaksi antara publik dengan Badan Pengusaha Batam</p>
                 <p class="pt-3 font-weight-bold">Media Sosial Kami </p>
-                <i class="fa fa-facebook-official" aria-hidden="true"></i>
-                <i class="fa fa-twitter" aria-hidden="true"></i>
-                <i class="fa fa-instagram" aria-hidden="true"></i>
-                <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                <div class="container w-25 float-left">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                        </div>
+                        <div class="col-md-3">
+                            <i class="fa fa-twitter" aria-hidden="true"></i>
+                        </div>
+                        <div class="col-md-3">
+                            <i class="fa fa-instagram" aria-hidden="true"></i>
+                        </div>
+                        <div class="col-md-3">
+                            <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="col-lg-4 mt-5 text-white" style="border-color: white;">
+        <div class="col-lg-6 mt-5 text-white">
             <div class="row">
                 <div class="col-lg-6">
-                    <h5 style="font-weight-bold">Kantor Batam</h5>
+                    <h5 class="font-weight-bold">Kantor Batam</h5>
                     <p>
                         Jalan Ibnu Sutowo No. 1 Batam Center
                         <br>Batam Kepulauan Riau, Indonesia, 29400
                         <br>+628778-462-047 / 462 048
                         <br>humas@bpbatam.go.id
+                    <p><i class="fa fa-map-marker" aria-hidden="true"></i>Petunjuk</p>
                     </p>
                 </div>
                 <div class="col-lg-6">
-                    <h5 style="font-weight-bold">Kantor Batam</h5>
+                    <h5 class="font-weight-bold">Jam Operasional</h5>
                     <p>
-                        Jalan Ibnu Sutowo No. 1 Batam Center
-                        <br>Batam Kepulauan Riau, Indonesia, 29400
-                        <br>+628778-462-047 / 462 048
-                        <br>humas@bpbatam.go.id
+                        Senin - Jumat
+                        <br> 09.00 - 16.00
                     </p>
                 </div>
-                <div class="col-lg-6">
-                    <h5 style="font-weight-bold">Kantor Batam</h5>
+                <div class="col-lg-6 mt-3">
+                    <h5 class="font-weight-bold">Kantor Perwakilan Jakarta</h5>
+
+                    Jalan Ibnu Sutowo No. 1 Batam Center
+                    <br>Batam Kepulauan Riau, Indonesia, 29400
+                    <br>+628778-462-047 / 462 048
+                    <br>humas@bpbatam.go.id
+                    <p><i class="fa fa-map-marker" aria-hidden="true"></i>Petunjuk</p>
+                </div>
+                <div class="col-lg-6 mt-3" style="list-style-type:none">
+                    <h5 class="font-weight-bold">Situs Terkait</h5>
                     <p>
-                        Jalan Ibnu Sutowo No. 1 Batam Center
-                        <br>Batam Kepulauan Riau, Indonesia, 29400
-                        <br>+628778-462-047 / 462 048
-                        <br>humas@bpbatam.go.id
+                        <li><i class="fa fa-caret-square-o-right" aria-hidden="true"></i>
+                            Gift of BIFZA</li>
+                        <li><i class="fa fa-caret-square-o-right" aria-hidden="true"></i>
+                            Batam Single Window</li>
+                        <li> <i class="fa fa-caret-square-o-right" aria-hidden="true"></i>
+                            Kalkulator UWT</li>
+                        <li><i class="fa fa-caret-square-o-right" aria-hidden="true"></i>
+                            Sistem Perizinan Lalu Lintas Barang</li>
+                        <li><i class="fa fa-caret-square-o-right" aria-hidden="true"></i>
+                            Sistem Pengadaan Secara Elektronik</li>
+                        <li><i class="fa fa-caret-square-o-right" aria-hidden="true"></i>
+                            Layanan Data dan Informasi</li>
                     </p>
                 </div>
             </div>
