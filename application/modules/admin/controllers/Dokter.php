ddf9b6d89bf476c8bc802e9e447eab018fb407ed
DESKTOP-7NBOJSE\Random <vantri_antonius@yahoo.co.id>
2023-12-18 17:19:01 +0700

    fitur tambah dokter

--- application/modules/admin/controllers/Dokter.php
+++ application/modules/admin/controllers/Dokter.php
@@ -1,15 +1,10 @@
-<?php class Dashboard extends CI_Controller
+<?php class Dokter extends CI_Controller
 {
     function __construct()
     {
         parent::__construct();
     }
     function index()
-    {
-        $data['title'] = 'Dashboard Admin';
-        $this->load->view('v_dashboard', $data);
-    }
-    function dokter()
     {
         $data['title'] = 'Dokter';
         $this->load->view('v_dokter', $data);
