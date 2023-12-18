223ca29e0b8a95509f6bdcd18723d2b2cbecba33
DESKTOP-7NBOJSE\Random <vantri_antonius@yahoo.co.id>
2023-12-18 21:01:47 +0700

    datatable dokter

--- application/modules/admin/views/v_dokter.php
+++ application/modules/admin/views/v_dokter.php
@@ -23,6 +23,13 @@
     <!-- bootstrap-progressbar -->
     <link href="<?= base_url('assets/assets-admin/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css'); ?>" rel="stylesheet">
 
+    <!-- DataTables -->
+    <link href="<?= base_url('assets/assets-admin/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/assets-admin/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/assets-admin/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/assets-admin/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/assets-admin/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css'); ?>" rel="stylesheet">
+
 
 
     <!-- JQVMap -->
@@ -133,6 +140,23 @@
 
     <!-- Custom Theme Scripts -->
     <script src="<?= base_url('assets/assets-admin/build/js/custom.min.js'); ?>"></script>
+
+    <!-- DataTables -->
+    <script src="<?= base_url('assets/assets-admin/vendors/datatables.net/js/jquery.dataTables.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/assets-admin/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/assets-admin/vendors/datatables.net-buttons/js/dataTables.buttons.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/assets-admin/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/assets-admin/vendors/datatables.net-buttons/js/buttons.flash.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/assets-admin/vendors/datatables.net-buttons/js/buttons.html5.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/assets-admin/vendors/datatables.net-buttons/js/buttons.print.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/assets-admin/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js'); ?>"></script>
+    <script src="<?= base_url('assets-assets-admin/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/assets-admin/vendors/datatables.net-responsive/js/dataTables.responsive.min.js'); ?>"></script>
+    <script src="../vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
+    <script src="../vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
+    <script src="../vendors/jszip/dist/jszip.min.js"></script>
+    <script src="../vendors/pdfmake/build/pdfmake.min.js"></script>
+    <script src="../vendors/pdfmake/build/vfs_fonts.js"></script>
 
 </body>
 
