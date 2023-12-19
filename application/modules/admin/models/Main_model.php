f5f4abc453f6d177bcac5d7427fbd6711cb117b7
DESKTOP-7NBOJSE\Random <vantri_antonius@yahoo.co.id>
2023-12-19 16:17:14 +0700

    file bagan

--- application/modules/admin/models/Main_model.php
+++ application/modules/admin/models/Main_model.php
@@ -8,7 +8,7 @@
     {
         $this->db->where('username', $username);
         $this->db->where('password', $password);
-        $query = $this->db->get('user');
+        $query = $this->db->get('admin');
 
         if ($query->num_rows() > 0) {
             # code...
