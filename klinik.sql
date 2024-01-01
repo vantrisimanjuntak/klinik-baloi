cb29702f1dbf5b99a1c1408babf40b005a718d1c
DESKTOP-7NBOJSE\Random <vantri_antonius@yahoo.co.id>
2024-01-02 01:05:29 +0700

    database

--- klinik.sql
+++ klinik.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Dec 31, 2023 at 06:35 PM
--- Server version: 8.0.30
--- PHP Version: 8.0.30
+-- Host: 127.0.0.1
+-- Generation Time: Jan 01, 2024 at 07:04 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -15,7 +15,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `klinik`
@@ -28,11 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `admin`
@@ -48,12 +48,12 @@ INSERT INTO `admin` (`id`, `nama`, `username`, `password`) VALUES
 --
 
 CREATE TABLE `dokter` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `tgl_pelayanan` date NOT NULL,
   `jam_pelayanan` time NOT NULL,
   `jabatan` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `dokter`
@@ -69,8 +69,8 @@ INSERT INTO `dokter` (`id`, `nama`, `tgl_pelayanan`, `jam_pelayanan`, `jabatan`)
 --
 
 CREATE TABLE `pasien` (
-  `no_mr` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `no_mr` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -79,23 +79,24 @@ CREATE TABLE `pasien` (
 --
 
 CREATE TABLE `pendaftar` (
-  `id` int NOT NULL,
-  `id_shadow` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `nama` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `no_ktp` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `no_hp` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `jenis_kelamin` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `id` int(11) NOT NULL,
+  `id_shadow` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `no_ktp` varchar(30) NOT NULL,
+  `no_hp` text NOT NULL,
+  `jenis_kelamin` varchar(20) NOT NULL,
   `alamat` text NOT NULL,
   `password` varchar(100) NOT NULL,
   `status` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `pendaftar`
 --
 
 INSERT INTO `pendaftar` (`id`, `id_shadow`, `nama`, `no_ktp`, `no_hp`, `jenis_kelamin`, `alamat`, `password`, `status`) VALUES
-(22, '58268b09e1d296f4', 'Vantri A Simanjuntak', '1209202410980002', '085358904344', 'Pria', 'Kisaran', 'f6fdffe48c908deb0f4c3bd36c032e72', 'Aktif');
+(22, '58268b09e1d296f4', 'Vantri A Simanjuntak', '1209202410980002', '085358904344', 'Pria', 'Kisaran', 'f6fdffe48c908deb0f4c3bd36c032e72', 'Aktif'),
+(23, '76a8685e188b00ca', 'Findy Christy', '120901012123123', '085312390123', 'Wanita', 'Kisaran', '4297f44b13955235245b2497399d7a93', 'Pending');
 
 -- --------------------------------------------------------
 
@@ -104,14 +105,14 @@ INSERT INTO `pendaftar` (`id`, `id_shadow`, `nama`, `no_ktp`, `no_hp`, `jenis_ke
 --
 
 CREATE TABLE `user` (
-  `no_ktp` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `nama` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `no_ktp` varchar(20) NOT NULL,
+  `nama` varchar(100) NOT NULL,
   `alamat` text NOT NULL,
   `jenis_kelamin` varchar(20) NOT NULL,
   `status` varchar(20) NOT NULL,
   `no_hp` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `user`
@@ -162,13 +163,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `dokter`
 --
 ALTER TABLE `dokter`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `pendaftar`
 --
 ALTER TABLE `pendaftar`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
